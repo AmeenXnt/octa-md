618385e6a2104613f6a19dae590d4f7578844ced
AmeenXnt <ameenserbots@gmail.com>
2024-08-26 20:14:38 +0530

    Update AddPlugin.js

--- plugins/AddPlugin.js
+++ plugins/AddPlugin.js
@@ -1,6 +1,6 @@
-const fs = require('fs')
-const path = require('path')
-const { eypz, commands } = require('../command')
+const fs = require('fs');
+const path = require('path');
+const { eypz, commands } = require('../command');
 
 eypz({
     pattern: "addplugin",
@@ -8,26 +8,20 @@ eypz({
     category: "owner",
     filename: __filename
 },
-
-
-async(conn, mek, m, { reply, quoted, args }) => {
+async (conn, mek, m, { reply, quoted, args }) => {
     try {
-        
-        if (!args[0]) return reply('_🙊Please Give Me A Name_\nEg:\n*addplugin AmeenInt*')
+        if (!args[0]) return reply('_🙊Please Give Me A Name_\nEg:\n*addplugin AmeenInt*');
 
-      
-        const AmeenXnt = args[0].trim()
-        const filePath = path.join(__dirname, '../plugins', `${AmeenXnt}.js`)
+        const AmeenXnt = args[0].trim();
+        const filePath = path.join(__dirname, '../plugins', `${AmeenXnt}.js`);
 
- 
-        if (!quoted) return reply('_Replay To A Js Code For Octa Bot Plugin🙄._')
+        if (!quoted || !quoted.text) return reply('_Replay To A Js Code For Octa Bot Plugin🙄._');
 
+        fs.writeFileSync(filePath, quoted.text, 'utf8');
 
-        fs.writeFileSync(filePath, quoted.text, 'utf8')
-
-        reply(`*Plugin Saved✅*\n\n> Plugin *${AmeenXnt}.js*`)
+        reply(`*Plugin Saved✅*\n\n> Plugin *${AmeenXnt}.js*`);
     } catch (e) {
-        console.log(e)
-        reply(`_Failed To Save The Plugin:_\nReason:\n ${e.message}`)
+        console.log(e);
+        reply(`_Failed To Save The Plugin:_\nReason:\n ${e.message}`);
     }
-})
+});
