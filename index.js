2586373ab40ff4a408338304a5a9b8fec350a45e
AmeenXnt <ameenserbots@gmail.com>
2024-08-27 02:09:49 +0530

    sms Fixed 🌸

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const {
     Browsers
 } = require('@whiskeysockets/baileys');
 
-const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson } = require('./lib/functions');
+const { sms, getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson } = require('./lib/functions');
 const fs = require('fs');
 const P = require('pino');
 const config = require('./config');
@@ -82,12 +82,9 @@ async function connectToWA() {
     conn.ev.on('creds.update', saveCreds);
 
     conn.ev.on('messages.upsert', async (mek) => {
-        mek = mek.messages[0];
-        if (!mek.message) return;
-        mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message;
-        if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true") {
-            await conn.readMessages([mek.key]);
-        }
+    mek = mek.messages[0];
+    if (!mek.message) return;
+    mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
 
         const m = sms(conn, mek);
         const type = getContentType(mek.message);
